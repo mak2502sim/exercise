e938b2c760847b9d063d45f5db87c7cb4374d325
Pyatovskiy Maxim <mak2502sim@gmail.com>
2022-08-08 12:03:26 +0300

    add insertion sort

---
+++ src/grokking_algorithms/chapter_2/insertion_sort.java
@@ -0,0 +1,26 @@
+package grokking_algorithms.chapter_2;
+
+import java.util.Arrays;
+
+public class insertion_sort {
+    public static void main(String[] args) {
+        int[] array = new int[]{2, 5, 65, 844, 0, 15, 32, 451, 5, 1, 100};
+
+        System.out.println(Arrays.toString(insertionSort(array)));
+    }
+
+    public static int[] insertionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int value = array[i];
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                if (value < array[j])
+                    array[j + 1] = array[j];
+                else
+                    break;
+            }
+            array[j + 1] = value;
+        }
+        return array;
+    }
+}
