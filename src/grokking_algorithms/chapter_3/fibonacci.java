0d7fa7a6b32a8b5b2caf1448ca17db462968c11d
Pyatovskiy Maxim <mak2502sim@gmail.com>
2022-08-08 12:50:35 +0300

    add recursive fibonacci algorithm with memoization

---
+++ src/grokking_algorithms/chapter_3/fibonacci.java
@@ -0,0 +1,24 @@
+package grokking_algorithms.chapter_3;
+
+import java.util.Arrays;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        int n = 60;
+        long[] mem = new long[n + 1];
+        Arrays.fill(mem, -1);
+
+        System.out.println(fib(n, mem));
+    }
+
+    public static long fib(int n, long[] mem) {
+        if (mem[n] != -1)
+            return mem[n];
+
+        if (n <= 1)
+            return n;
+
+        mem[n] = fib(n - 1, mem) + fib(n - 2, mem);
+        return mem[n];
+    }
+}
