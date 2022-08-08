063cb9e586b66d744bd88bfe7703cd6b576d3544
Pyatovskiy Maxim <mak2502sim@gmail.com>
2022-08-08 11:44:59 +0300

    add simple search

---
+++ src/grokking_algorithms/chapter_1/simple_search.java
@@ -0,0 +1,21 @@
+package grokking_algorithms.chapter_1;
+
+public class simple_search {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+
+        System.out.println(simpleSearch(array, 100));
+    }
+
+    public static int simpleSearch(int[] array, int item) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == item)
+                return i;
+        }
+
+        return -1;
+    }
+}
