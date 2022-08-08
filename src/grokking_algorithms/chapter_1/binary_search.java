781e0edd99eb07126d209216d57c837c9e912bea
Pyatovskiy Maxim <mak2502sim@gmail.com>
2022-08-08 10:33:28 +0300

    first commit

---
+++ src/grokking_algorithms/chapter_1/binary_search.java
@@ -0,0 +1,32 @@
+package grokking_algorithms.chapter_1;
+
+public class binary_search {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+
+        System.out.println(binarySearch(array, 100));
+    }
+
+    public static int binarySearch(int[] array, int item) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int guess = array[mid];
+
+            if (guess == item)
+                return mid;
+
+            if (guess > item)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return -1;
+    }
+}
