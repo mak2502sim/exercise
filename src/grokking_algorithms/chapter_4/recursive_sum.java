bf3c0d86eae09c4ee6fcc972d8b3172106855780
Pyatovskiy Maxim <mak2502sim@gmail.com>
2022-08-09 12:20:17 +0300

    add chapter 4

---
+++ src/grokking_algorithms/chapter_4/recursive_sum.java
@@ -0,0 +1,19 @@
+package grokking_algorithms.chapter_4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class recursive_sum {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2, 5, 65, 844, 0, 15, 32, 451, 5, 1, 100));
+
+        System.out.println(sum(list));
+    }
+
+    public static int sum(ArrayList<Integer> list) {
+        if (list.size() == 1)
+            return list.get(0);
+        else
+            return list.remove(0) + sum(list);
+    }
+}
