41e9bf1cfa8bbfdb57da6a98e467aeb92bfbd784
Pyatovskiy Maxim <mak2502sim@gmail.com>
2022-08-11 12:08:38 +0300

    Add quick sort

---
+++ src/grokking_algorithms/chapter_4/QuickSort.java
@@ -0,0 +1,41 @@
+package grokking_algorithms.chapter_4;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] array = new int[]{2, 5, 65, 844, 0, 15, 32, 451, 5, 1, 100};
+        quickSort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void quickSort(int[] array, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(array, begin, end);
+
+            quickSort(array, begin, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(int[] array, int begin, int end) {
+        int pivot = array[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (array[j] <= pivot) {
+                i++;
+
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        int temp = array[i + 1];
+        array[i + 1] = array[end];
+        array[end] = temp;
+
+        return i + 1;
+    }
+}
