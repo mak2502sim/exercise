35d4ea339a0b28cc859c09ec1756af094ab3dfd1
Pyatovskiy Maxim <mak2502sim@gmail.com>
2022-08-11 12:49:15 +0300

    Add counting sort

---
+++ src/grokking_algorithms/chapter_4/CountingSort.java
@@ -0,0 +1,30 @@
+package grokking_algorithms.chapter_4;
+
+import java.util.Arrays;
+
+public class CountingSort {
+    public static void main(String[] args) {
+        int[] array = new int[]{2, 5, 65, 844, 0, 15, 32, 451, 5, 1, 100};
+
+        System.out.println(Arrays.toString(countingSort(array, Arrays.stream(array).max().getAsInt())));
+    }
+
+    public static int[] countingSort(int[] sourceArray, int maxValue) {
+        int[] counts = new int[maxValue + 1];
+
+        for (int i :
+                sourceArray) {
+            counts[i]++;
+        }
+
+        int[] sortedArray = new int[sourceArray.length];
+        int sortedCounts = 0;
+        for (int i = 0; i < counts.length; i++) {
+            int count = counts[i];
+            for (int j = 0; j < count; j++) {
+                sortedArray[sortedCounts++] = i;
+            }
+        }
+        return sortedArray;
+    }
+}
